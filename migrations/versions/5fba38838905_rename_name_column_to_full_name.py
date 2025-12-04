efd485086ccf0bd6159d383dbc37c22577cc7b7b
Kibet Chumo <kibet.chumo@student.moringaschool.com>
2025-12-04 15:45:12 +0300

    manual migrations

---
+++ migrations/versions/5fba38838905_rename_name_column_to_full_name.py
@@ -0,0 +1,24 @@
+"""Rename name column to full_name
+
+Revision ID: 5fba38838905
+Revises: 791279dd0760
+Create Date: 2025-12-04 15:44:13.105764
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5fba38838905'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
